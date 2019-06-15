c3b011139199ca07e09aa990aa42b0a4cb5d751d
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-06-15 03:08:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,13 +6,13 @@ class App < Sinatra::Base
   end
 
   post '/newteam' do
-    @name = params[:name]
-    @coach = params[:coach]
-    @pg = params[:pg]
-    @sg = params[:sg]
-    @pf = params[:pf]
-    @sf = params[:sf]
-    @c = params[:c]
+    @name = params["name"]
+    @coach = params["coach"]
+    @pg = params["pg"]
+    @sg = params["sg"]
+    @pf = params["pf"]
+    @sf = params["sf"]
+    @c = params["c"]
     erb :team
   end
 
